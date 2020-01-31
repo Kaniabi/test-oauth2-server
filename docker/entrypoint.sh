fad1eeecaa7d8d83bf43c103abc7829cfc31a50e
Alexandre Andrade <kaniabi@gmail.com>
2020-01-31 16:09:41 -0300

    IMP: Waiting for database before starting the application.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,25 @@
 
 set -e
 
+wait_for_db () {
+  # Wait until postgresql is up and running.
+  # Looping pg_isready, because the database can receive a fast-restart
+  # command during the first init, and pg_isready will then immediately quit
+  # with "rejecting connections" error.
+  for _ in $(seq 5); do
+    if ! pg_isready -h $DB_HOST -t 360
+    then
+      sleep 3  # Wait 3s between retries
+    else
+      break
+    fi
+  done
+}
+
 cd /app
 if [ $# -eq 0 ]; then
     # No arguments, simply run the app
+    wait_for_db
     python /app/manage.py migrate
     python /app/manage.py initialize
     nginx -g "daemon off;" &                            # Start http web-server
